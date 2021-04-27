8c8310a1f5e3112461186b144e83f2588a926fab
murata0531 <coless115@gmail.com>
2021-04-27 10:08:04 +0900

    draw

--- src/17_draw_plane/boot.s
+++ src/17_draw_plane/boot.s
@@ -115,3 +115,53 @@ ACPI_DATA:										; ACPI data
 %include	"../modules/real/kbc.s"
 %include	"../modules/real/lba_chs.s"
 %include	"../modules/real/read_lba.s"
+
+;************************************************************************
+;	ブート処理の第2ステージ
+;************************************************************************
+stage_2:
+		;---------------------------------------
+		; 文字列を表示
+		;---------------------------------------
+		cdecl	puts, .s0						; puts(.s0);
+
+		;---------------------------------------
+		; ドライブ情報を取得
+		;---------------------------------------
+		cdecl	get_drive_param, BOOT			; get_drive_param(DX, BOOT.CYLN);
+		cmp		ax, 0							; if (0 == AX)
+.10Q:	jne		.10E							; {
+.10T:	cdecl	puts, .e0						;   puts(.e0);
+		call	reboot							;   reboot(); // 再起動
+.10E:											; }
+
+		;---------------------------------------
+		; ドライブ情報を表示
+		;---------------------------------------
+		mov		ax, [BOOT + drive.no]			; AX = ブートドライブ;
+		cdecl	itoa, ax, .p1, 2, 16, 0b0100	; 
+		mov		ax, [BOOT + drive.cyln]			; 
+		cdecl	itoa, ax, .p2, 4, 16, 0b0100	; 
+		mov		ax, [BOOT + drive.head]			; AX = ヘッド数;
+		cdecl	itoa, ax, .p3, 2, 16, 0b0100	; 
+		mov		ax, [BOOT + drive.sect]			; AX = トラックあたりのセクタ数;
+		cdecl	itoa, ax, .p4, 2, 16, 0b0100	; 
+		cdecl	puts, .s1
+
+		;---------------------------------------
+		; 次のステージへ移行
+		;---------------------------------------
+		jmp		stage_3rd						; 次のステージへ移行
+
+		;---------------------------------------
+		; データ
+		;---------------------------------------
+.s0		db	"2nd stage...", 0x0A, 0x0D, 0
+
+.s1		db	" Drive:0x"
+.p1		db	"  , C:0x"
+.p2		db	"    , H:0x"
+.p3		db	"  , S:0x"
+.p4		db	"  ", 0x0A, 0x0D, 0
+
+.e0		db	"Can't get drive parameter.", 0
